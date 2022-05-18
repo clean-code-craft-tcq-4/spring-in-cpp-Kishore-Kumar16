4973c2a6b9682c204482e6f3f2ef966928b942e5
Kishore-Kumar16 <105337698+Kishore-Kumar16@users.noreply.github.com>
2022-05-18 17:08:25 +0530

    Stats structure update

--- stats.h
+++ stats.h
@@ -1,4 +1,14 @@
 #include <vector>
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Stats{
+    double average, min, max;
+};
 
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<___>& );
